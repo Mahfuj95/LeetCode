991c9c306e303f376529e1375448709b5d628cf3
Mahfuj95 <95295539+Mahfuj95@users.noreply.github.com>
2022-02-08 12:47:51 +0600

    Time: 2 ms (54.83%), Space: 5.9 MB (75.31%) - LeetHub

--- 258-add-digits/258-add-digits.cpp
+++ 258-add-digits/258-add-digits.cpp
@@ -1,20 +1,8 @@
 class Solution {
-public:
-    
-    int findSum(int num, int sum){
-        if(num > 0){
-            sum += num%10;
-            num /= 10;
-            return findSum(num, sum);
-        }
-        
-        if(sum > 9){
-            return findSum(sum, 0);
-        }
-        return sum;
-    }
-    
+public:  
     int addDigits(int num) {
-        return findSum(num, 0);
+        if(num == 0) return 0;
+        else if(num%9 == 0) return 9;
+        return num%9;
     }
 };
